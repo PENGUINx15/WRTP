149aeff35579044019e43820701d7154ecae4db8
PENGUINx13 <demchuk250706@gmail.com>
2025-10-18 20:19:08 +0200

    1.2.0 - up to 1.19.4 bug fix

--- src/main/java/me/penguinx13/wrtp/TeleportEvent.java
+++ src/main/java/me/penguinx13/wrtp/TeleportEvent.java
@@ -9,14 +9,11 @@ import org.bukkit.World;
 import org.bukkit.block.Biome;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.UUID;
+
+import java.util.*;
 
 public class TeleportEvent implements Listener {
+
     private final ConfigManager config;
     private final Map<UUID, Long> cooldowns;
     private final int cooldown;
@@ -26,168 +23,189 @@ public class TeleportEvent implements Listener {
         this.cooldown = config.getConfig("config.yml").getInt("cooldown");
         this.cooldowns = new HashMap<>();
     }
+
     public void teleportPlayer(Player player, String channel) {
         teleportPlayer(player, channel, false);
     }
 
     public void teleportPlayer(Player player, String channel, Boolean ignoreCooldown) {
-        String type = config.getConfig("config.yml").getString("channels."+channel+".type");
+        String type = config.getConfig("config.yml").getString("channels." + channel + ".type");
+
         if (Boolean.TRUE.equals(ignoreCooldown) || isCooldownExpired(player)) {
-            switch (type.toUpperCase()) {
+            switch (Objects.requireNonNull(type).toUpperCase()) {
                 case "DEFAULT":
                     defaultType(player, channel);
                     break;
-
                 case "NEARBY_PLAYERS":
                     nearbyType(player, channel);
                     break;
-
                 case "BIOME":
                     biomeType(player, channel);
                     break;
                 default:
-                    MessageManager.sendMessage(player, config.getConfig("config.yml").getString("messages.unknownType").replace("{type}", type));
-            }
-        } else{
-            MessageManager.sendMessage(player, config.getConfig("config.yml").getString("messages.cooldown").replace("{time}", String.valueOf(getRemainingCooldown(player))));
-        }
-    }
-    private void defaultType(Player player, String channel) {
-        int minX = config.getConfig("config.yml").getInt("channels."+channel+".range.minX");
-        int maxX = config.getConfig("config.yml").getInt("channels."+channel+".range.maxX");
-        int minZ = config.getConfig("config.yml").getInt("channels."+channel+".range.minZ");
-        int maxZ = config.getConfig("config.yml").getInt("channels."+channel+".range.maxZ");
-        World world = Bukkit.getWorld(config.getConfig("config.yml").getString("channels."+channel+".world"));
-        int x, z, y;
-        do {
-            x = getRandom(minX, maxX);
-            z = getRandom(minZ, maxZ);
-            y = player.getWorld().getHighestBlockYAt(x, z);
-
-        } while (isBlacklistedBlock(player.getWorld().getBlockAt(x, y-1, z).getType(), channel) &&
-                player.getWorld().getBlockAt(x, y + 1, z).getType() == Material.AIR &&
-                player.getWorld().getBlockAt(x, y + 2, z).getType() == Material.AIR);
-
-        setCooldown(player);
-
-        player.setFallDistance(0.0f);
-        player.teleport(new Location(world, x + 0.5, y + 1, z + 0.5));
-        assert world != null;
-        MessageManager.sendMessage(player,
-                config.getConfig("config.yml").getString("channels."+channel+".message")
-                        .replace("{x}", String.valueOf(x))
-                        .replace("{z}", String.valueOf(z))
-                        .replace("{y}", String.valueOf(y))
-                        .replace("{world}", world.getName())
-        );
-
-    }
-    private void nearbyType(Player player, String channel) {
-        int min = config.getConfig("config.yml").getInt("channels."+channel+".nearbyRange.min");
-        int max = config.getConfig("config.yml").getInt("channels."+channel+".nearbyRange.max");
-        int x, z, y;
-        int minOnline = config.getConfig("config.yml").getInt("channels."+channel+".minOnline");
-        if (Bukkit.getOnlinePlayers().size() >= minOnline) {
-            int range = getRandom(min, max);
-            Player targetPlayer = findRandomNearbyPlayer(player);
-            if(targetPlayer != null) {
-                double angle = Math.toRadians(new Random().nextDouble() * 360);
-                do {
-                    x = (int) (targetPlayer.getLocation().getX() + range * Math.cos(angle));
-                    z = (int) (targetPlayer.getLocation().getZ() + range * Math.sin(angle));
-
-                    y = targetPlayer.getWorld().getHighestBlockYAt(x, z);
-
-                } while (isBlacklistedBlock(player.getWorld().getBlockAt(x, y-1, z).getType(), channel) &&
-                        player.getWorld().getBlockAt(x, y + 1, z).getType() == Material.AIR &&
-                        player.getWorld().getBlockAt(x, y + 2, z).getType() == Material.AIR);
-                setCooldown(player);
-
-                player.setFallDistance(0.0f);
-                player.teleport(new Location(targetPlayer.getWorld(), x + 0.5, y + 1, z + 0.5));
-                MessageManager.sendMessage(player,
-                        (config.getConfig("config.yml").getString("channels."+channel+".message"))
-                                .replace("{x}", String.valueOf(x))
-                                .replace("{z}", String.valueOf(z))
-                                .replace("{y}", String.valueOf(y))
-                                .replace("{targetPlayer}", String.valueOf(targetPlayer.getName()))
-                                .replace("{world}", String.valueOf(targetPlayer.getWorld()))
-                );
-            } else {
-                MessageManager.sendMessage(player, config.getConfig("config.yml").getString("messages.noPlayerInWorld"));
+                    MessageManager.sendMessage(player, Objects.requireNonNull(config.getConfig("config.yml").getString("messages.unknownType")).replace("{type}", type));
             }
         } else {
-            MessageManager.sendMessage(player, config.getConfig("config.yml").getString("messages.noMinPlayers"));
+            MessageManager.sendMessage(player, Objects.requireNonNull(config.getConfig("config.yml").getString("messages.cooldown")).replace("{time}", String.valueOf(getRemainingCooldown(player))));
         }
     }
+
+    private void defaultType(Player player, String channel) {
+        int minX = config.getConfig("config.yml").getInt("channels." + channel + ".range.minX");
+        int maxX = config.getConfig("config.yml").getInt("channels." + channel + ".range.maxX");
+        int minZ = config.getConfig("config.yml").getInt("channels." + channel + ".range.minZ");
+        int maxZ = config.getConfig("config.yml").getInt("channels." + channel + ".range.maxZ");
+        World world = Bukkit.getWorld(Objects.requireNonNull(config.getConfig("config.yml").getString("channels." + channel + ".world")));
+
+        assert world != null;
+        Location safeLocation = findSafeLocation(world, minX, maxX, minZ, maxZ, channel, false);
+        if (safeLocation == null) {
+            MessageManager.sendMessage(player, config.getConfig("config.yml").getString("messages.noSafeLocation"));
+            return;
+        }
+
+        setCooldown(player);
+        player.setFallDistance(0.0f);
+        player.teleport(safeLocation);
+
+        MessageManager.sendMessage(player,
+                Objects.requireNonNull(config.getConfig("config.yml").getString("channels." + channel + ".message"))
+                        .replace("{x}", String.valueOf(safeLocation.getBlockX()))
+                        .replace("{y}", String.valueOf(safeLocation.getBlockY()))
+                        .replace("{z}", String.valueOf(safeLocation.getBlockZ()))
+                        .replace("{world}", world.getName()));
+    }
+
+    private void nearbyType(Player player, String channel) {
+        int minRange = config.getConfig("config.yml").getInt("channels." + channel + ".nearbyRange.min");
+        int maxRange = config.getConfig("config.yml").getInt("channels." + channel + ".nearbyRange.max");
+        int minOnline = config.getConfig("config.yml").getInt("channels." + channel + ".minOnline");
+
+        if (Bukkit.getOnlinePlayers().size() < minOnline) {
+            MessageManager.sendMessage(player, config.getConfig("config.yml").getString("messages.noMinPlayers"));
+            return;
+        }
+
+        Player targetPlayer = findRandomNearbyPlayer(player);
+        if (targetPlayer == null) {
+            MessageManager.sendMessage(player, config.getConfig("config.yml").getString("messages.noPlayerInWorld"));
+            return;
+        }
+
+        double angle = Math.toRadians(new Random().nextDouble() * 360);
+        int range = getRandom(minRange, maxRange);
+
+        int x = (int) (targetPlayer.getLocation().getX() + range * Math.cos(angle));
+        int z = (int) (targetPlayer.getLocation().getZ() + range * Math.sin(angle));
+
+        Location safeLocation = findSafeLocation(player.getWorld(), x, x, z, z, channel, false);
+        if (safeLocation == null) {
+            MessageManager.sendMessage(player, config.getConfig("config.yml").getString("messages.noSafeLocation"));
+            return;
+        }
+
+        setCooldown(player);
+        player.setFallDistance(0.0f);
+        player.teleport(safeLocation);
+
+        MessageManager.sendMessage(player,
+                Objects.requireNonNull(config.getConfig("config.yml").getString("channels." + channel + ".message"))
+                        .replace("{x}", String.valueOf(safeLocation.getBlockX()))
+                        .replace("{y}", String.valueOf(safeLocation.getBlockY()))
+                        .replace("{z}", String.valueOf(safeLocation.getBlockZ()))
+                        .replace("{targetPlayer}", targetPlayer.getName())
+                        .replace("{world}", String.valueOf(player.getWorld())));
+    }
+
     private void biomeType(Player player, String channel) {
-        int minX = config.getConfig("config.yml").getInt("channels."+channel+".range.minX");
-        int maxX= config.getConfig("config.yml").getInt("channels."+channel+".range.maxX");
-        int minZ = config.getConfig("config.yml").getInt("channels."+channel+".range.minZ");
-        int maxZ = config.getConfig("config.yml").getInt("channels."+channel+".range.maxZ");
-        World world = Bukkit.getWorld(config.getConfig("config.yml").getString("channels."+channel+".world"));
+        int minX = config.getConfig("config.yml").getInt("channels." + channel + ".range.minX");
+        int maxX = config.getConfig("config.yml").getInt("channels." + channel + ".range.maxX");
+        int minZ = config.getConfig("config.yml").getInt("channels." + channel + ".range.minZ");
+        int maxZ = config.getConfig("config.yml").getInt("channels." + channel + ".range.maxZ");
+        World world = Bukkit.getWorld(Objects.requireNonNull(config.getConfig("config.yml").getString("channels." + channel + ".world")));
+
+        assert world != null;
+        Location safeLocation = findSafeLocation(world, minX, maxX, minZ, maxZ, channel, true);
+        if (safeLocation == null) {
+            MessageManager.sendMessage(player, config.getConfig("config.yml").getString("messages.noSafeLocation"));
+            return;
+        }
+
+        Biome biome = safeLocation.getWorld().getBlockAt(safeLocation.getBlockX(), safeLocation.getBlockY() - 1, safeLocation.getBlockZ()).getBiome();
+        setCooldown(player);
+        player.setFallDistance(0.0f);
+        player.teleport(safeLocation);
+
+        MessageManager.sendMessage(player,
+                Objects.requireNonNull(config.getConfig("config.yml").getString("channels." + channel + ".message"))
+                        .replace("{biome}", biome.name()));
+    }
+
+    private Location findSafeLocation(World world, int minX, int maxX, int minZ, int maxZ, String channel, boolean checkBiome) {
         int x, z, y;
+        int attempts = 0;
 
         do {
             x = getRandom(minX, maxX);
             z = getRandom(minZ, maxZ);
-            y = player.getWorld().getHighestBlockYAt(x, z);
+            y = world.getHighestBlockYAt(x, z);
 
-        } while (!isBiome(world.getBlockAt(x, y - 1, z).getBiome(), channel) &&
-                world.getBlockAt(x, y + 1, z).getType() == Material.AIR &&
-                world.getBlockAt(x, y + 2, z).getType() == Material.AIR);
-        Biome biome = world.getBlockAt(x, y-1, z).getBiome();
-        setCooldown(player);
+            attempts++;
+            if (attempts > 100) {
+                return null;
+            }
 
-        player.setFallDistance(0.0f);
-        player.teleport(new Location(world, x + 0.5, y + 1, z + 0.5));
+        } while ((checkBiome && !isBiome(world.getBlockAt(x, y - 1, z).getBiome(), channel))
+                || !isSafeLocation(world, x, y, z));
+        Bukkit.getLogger().info("Location find, location " + new Location(world, x + 0.5, y + 1, z + 0.5));
+        return new Location(world, x + 0.5, y + 1, z + 0.5);
+    }
 
-        MessageManager.sendMessage(player,
-                config.getConfig("config.yml").getString("channels."+channel+".message")
-                        .replace("{biome}", biome.name())
-        );
+
+
+    private boolean isSafeLocation(World world, int x, int y, int z) {
+        Material below = world.getBlockAt(x, y, z).getType();
+        Material at = world.getBlockAt(x, y + 1, z).getType();
+        Material above = world.getBlockAt(x, y + 2, z).getType();
+        Bukkit.getLogger().info("Blocks" + below + " " + at + " " + above);
+        return !isBlacklistedBlock(below) && (at == Material.AIR || at == Material.SNOW) && above == Material.AIR;
+    }
+
+    private boolean isBlacklistedBlock(Material material) {
+        List<String> blacklistedBlocks = config.getConfig("config.yml").getStringList("blacklistedBlocks");
+        blacklistedBlocks = blacklistedBlocks.stream().map(String::toUpperCase).toList();
+        return blacklistedBlocks.contains(material.toString());
+    }
+
+    private boolean isBiome(Biome biome, String channel) {
+        List<String> biomes = config.getConfig("config.yml").getStringList("channels." + channel + ".biomes");
+        Bukkit.getLogger().info("Biome" + biome);
+        return biomes.stream().map(String::toUpperCase).toList().contains(biome.name());
     }
 
     private void setCooldown(Player player) {
-        long currentTime =  (System.currentTimeMillis() / 1000);
-        cooldowns.put(player.getUniqueId(), currentTime);
+        cooldowns.put(player.getUniqueId(), System.currentTimeMillis() / 1000);
     }
+
     private boolean isCooldownExpired(Player player) {
         long startTime = cooldowns.getOrDefault(player.getUniqueId(), 0L);
-        int elapsedTime = (int) ((System.currentTimeMillis() / 1000) - startTime);
-        return elapsedTime >= cooldown;
+        return ((System.currentTimeMillis() / 1000) - startTime) >= cooldown;
     }
+
     private int getRemainingCooldown(Player player) {
         long startTime = cooldowns.getOrDefault(player.getUniqueId(), 0L);
-        int elapsedTime = (int) ((System.currentTimeMillis() / 1000) - startTime);
-        return Math.max(0, cooldown - elapsedTime);
+        return Math.max(0, cooldown - (int) ((System.currentTimeMillis() / 1000) - startTime));
     }
 
     private int getRandom(int min, int max) {
         return min + new Random().nextInt(max - min + 1);
     }
 
-    private boolean isBlacklistedBlock(Material material, String channel) {
-        List<String> blacklistedBlocks = config.getConfig("config.yml").getStringList("channels." + channel + ".blacklistedBlocks");
-        blacklistedBlocks = blacklistedBlocks.stream().map(String::toUpperCase).toList();
-        return blacklistedBlocks.contains(material.toString());
-    }
-    private boolean isBiome(Biome biome, String channel) {
-        List<String> biomes = config.getConfig("config.yml").getStringList("channels." + channel + ".biomes");
-        biomes = biomes.stream().map(String::toUpperCase).toList();
-        return biomes.contains(biome.name());
-    }
-
     private Player findRandomNearbyPlayer(Player player) {
-        World world = player.getWorld();
-
-        List<Player> nearbyPlayers = new ArrayList<>(world.getPlayers());
-
+        List<Player> nearbyPlayers = new ArrayList<>(player.getWorld().getPlayers());
         if (!nearbyPlayers.isEmpty()) {
             return nearbyPlayers.get(new Random().nextInt(nearbyPlayers.size()));
-        } else {
-            return null;
         }
+        return null;
     }
-
 }
